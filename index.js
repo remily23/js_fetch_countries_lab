24708f4d3e8f38b051f493bb49eba13124b2d5ed
Emily Reid <emily.reid31@gmail.com>
2023-11-30 20:29:58 +0000

    reattempted MVP following lab review adding notes, tested displaying several different single countries

--- index.js
+++ index.js
@@ -1,61 +1,57 @@
-// console.log("hello!");
+// arrow function 
+const displayCountryInfo = (country) => {
+    const countryDiv = document.createElement("div");
 
-// Fetch a single country
+    // PTags for each element of the country to display as a para, innerText to return all text within the element
+    const namePTag = document.createElement("p");
+    namePTag.innerText = country.name.common;
+
+    const capitalPTag = document.createElement("p");
+    capitalPTag.innerText = country.capital;
+
+    const populationPTag = document.createElement("p");
+    populationPTag.innerText = country.population;
+
+    // Ul tag for list of country languages 
+    const languagesUlTag = document.createElement("ul");
+    Object.values(country.languages).forEach((language) => {
+        const languageLi = document.createElement("li");
+        languageLi.innerText = language;
+        // appendChild to add the languagesULTag to the Li
+        languagesUlTag.appendChild(languageLi);
+    })
+    // appendChild each element to add a node to the end of the list, returns appended node
+    countryDiv.appendChild(namePTag);
+    countryDiv.appendChild(capitalPTag);
+    countryDiv.appendChild(populationPTag);
+    countryDiv.appendChild(languagesUlTag);
+    // horizontal line 
+    countryDiv.appendChild(document.createElement("hr"));
+
+    document.querySelector("#single-country").appendChild(countryDiv);
+
+}
+
+    // Fetch a single country
 const getCountryByName = async(countryName) => {
-    countryName.preventDefault();
+
     const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`)
-    const jsonData = await response.json();
-    console.log(jsonData);
+    const arrayOfCountries = await response.json();
+    console.log(arrayOfCountries[0]);
 
-const countryInfo = document.createElement("h2");
-const listName = document.createElement("h3");
-listName.textContent = jsonData[0].name.common;
+    // to display on html, takes country object from arrayOfCountries into the displayCountryInfo
+    displayCountryInfo(arrayOfCountries[0])
+}  
 
-const countries = document.querySelector("#countries");
-countries.appendChild(countryInfo);
-countryInfo.appendChild(listName);
+    // Hardcoded for MVP
+    getCountryByName("australia");
 
-}
+// const getAllCountries = async () => {
+//     const response = await fetch(`https://restcountries.com/v3.1/all`);
+//     const getAllCountries = await response.json();
+//     allCountries.forEach((country) => {
+//         displayCountryInfo(country);
+//     })
+// };
 
-const onSubmit = (event) => {
-    event.preventDefault();
-    getCountryByName(event.target.countries.value);
-
-}
-
-const searchForm = document.querySelector("search");
-searchForm.addEventListener("submit", onSubmit);
-
-
-getCountryByName("australia");
-
-
-
-
-    // data.forEach((country) => {
-    //     const countrySection = document.getElementById("countriesSection");
-    //     const countryHeader = document.createElement("h2");
-    //     countryHeader.textContent = country.name.official;
-    //     countrySection.appendChild(countryHeader);
-    //     const populationHeader = document.createElement("h3");
-    //     populationHeader.textContent = "Population: " + country.population;
-    //     countrySection.appendChild(populationHeader);
-    // });
-
-
-    // const getAllCountries = async() => {
-    //     const response = await fetch ('https://restcountries.com/v3.1/all');
-    //     const jsonData = await response.json();
-    //     jsonData.forEach((country) => {
-    //         const countrySection = document.getElementById("countriesSection");
-    //         const countryHeader = document.createElement("h2");
-    //         countryHeader.textContent = country.name.official;
-    //         countrySection.appendChild(countryHeader);
-    //         const populationHeader = document.createElement("h3");
-    //         populationHeader.textContent = "Population: " + country.population;
-    //         countrySection.appendChild(populationHeader);
-    //         console.log(country.name.official)
-    //     });
-    
-
-    
+// const getAllCountries();
